f35cf253b5dc384acbc903ec2feb01e2a1a5a1ba
giupe <giupe@MSI>
2025-09-19 17:16:06 +0200

    Add project files.

---
+++ 19.3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace _19._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void StampaPesci(List<string> salmoni)
+            {
+                foreach (string salmone in salmoni)
+                {
+                    Console.WriteLine(salmone);
+                    
+                }
+                Console.WriteLine();
+            }
+            List<string> pesci = new List<string> { "chinnok", "coho", "pink", "sockeye" };
+            StampaPesci(pesci);
+            Console.WriteLine(pesci[2]);
+            Console.WriteLine();
+            pesci.Remove("coho");
+            Console.WriteLine();
+            StampaPesci(pesci);
+            pesci.Remove("chinnok");
+            Console.WriteLine();
+            StampaPesci(pesci);
+        }
+    }
+}
